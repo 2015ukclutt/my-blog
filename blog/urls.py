34a0b9195bd59231380f71e886a5507a9386d172
2015ukclutt <Matthew_Clutterbuck@yahoo.co.uk>
2020-06-08 17:27:22 +0100

    Updates to include CV page

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about/', views.about, name='about'),
+    path('cv/', views.cv, name='cv'),
     path('blog/', views.post_list, name='post_list'),
     path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
     path('blog/post/new/', views.post_new, name='post_new'),
